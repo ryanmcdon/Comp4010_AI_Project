bdc57a24d168b95b77162905bf6e3266c16b2d07
Stanny Huang <hstanny@gmail.com>
2025-10-01 15:45:44 -0400

    Screen Capping Python File

---
+++ ScreenCapture.py
@@ -0,0 +1,11 @@
+import pyautogui
+
+# Capture the entire screen
+screenshot = pyautogui.screenshot()
+
+# Save the screenshot to a file
+screenshot.save("full_screen_capture.png")
+
+# Capture a specific region (left, top, width, height)
+region_screenshot = pyautogui.screenshot(region=(100, 100, 500, 300))
+region_screenshot.save("region_capture.png")
